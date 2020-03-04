152bf3b6a5b43503d14101fa265d67676d9a4358
Afonso Matos <afonsolfmatos@gmail.com>
2020-03-04 16:16:41 +0000

    Remove unused repository methods

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/studentQuestion/StudentQuestionRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/studentQuestion/StudentQuestionRepository.java
@@ -12,13 +12,4 @@ import java.util.Optional;
 
 @Repository
 @Transactional
-public interface StudentQuestionRepository extends JpaRepository<StudentQuestion, Integer> {
-
-    @Query(value = "SELECT * FROM student_questions WHERE student_id = :studentId", nativeQuery = true)
-    List<StudentQuestion> getStudentQuestions(int studentId);
-
-
-    @Query(value = "SELECT * FROM student_questions s JOIN questions q ON s.question_id = q.id JOIN courses c ON q.course_id = c.id WHERE c.id = :courseId", nativeQuery = true)
-    List<StudentQuestion> getStudentQuestionsByCourse(int courseId);
-
-}
+public interface StudentQuestionRepository extends JpaRepository<StudentQuestion, Integer> {}
