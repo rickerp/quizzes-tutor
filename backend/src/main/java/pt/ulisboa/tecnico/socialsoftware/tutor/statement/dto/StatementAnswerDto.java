a189df803e769f358b05d51de1d6af349af1b20a
RafaelAlexandreIST <rafael.alexandre.roberto.figueiredo@tecnico.ulisboa.pt>
2020-04-15 04:17:19 +0100

    Added QuestionAnswerId and ClarificationDto to StatementAnswerDto #79

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/statement/dto/StatementAnswerDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/statement/dto/StatementAnswerDto.java
@@ -1,24 +1,34 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.statement.dto;
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuestionAnswer;
+import pt.ulisboa.tecnico.socialsoftware.tutor.clarification.domain.ClarificationRequest;
+import pt.ulisboa.tecnico.socialsoftware.tutor.clarification.dto.ClarificationRequestDto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class StatementAnswerDto implements Serializable {
+    private Integer questionAnswerId;
     private Integer timeTaken;
     private Integer sequence;
     private Integer optionId;
+    private List<ClarificationRequestDto> clarificationRequests = new ArrayList<>();
 
     public StatementAnswerDto(){}
 
     public StatementAnswerDto(QuestionAnswer questionAnswer) {
         this.timeTaken = questionAnswer.getTimeTaken();
         this.sequence = questionAnswer.getSequence();
+        this.questionAnswerId = questionAnswer.getId();
 
         if(questionAnswer.getOption() != null) {
             this.optionId = questionAnswer.getOption().getId();
         }
+        this.clarificationRequests = questionAnswer.getClarificationRequests().stream()
+                .map(ClarificationRequestDto::new).collect(Collectors.toList());
     }
 
     public Integer getOptionId() {
@@ -43,6 +53,22 @@ public class StatementAnswerDto implements Serializable {
 
     public void setSequence(Integer sequence) {
         this.sequence = sequence;
+    }
+
+    public Integer getQuestionAnswerId() {
+        return questionAnswerId;
+    }
+
+    public void setQuestionAnswerId(Integer questionAnswerId) {
+        this.questionAnswerId = questionAnswerId;
+    }
+
+    public List<ClarificationRequestDto> getClarificationRequests() {
+        return clarificationRequests;
+    }
+
+    public void setClarificationRequests(List<ClarificationRequestDto> clarificationRequests) {
+        this.clarificationRequests = clarificationRequests;
     }
 
     @Override
